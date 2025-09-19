cef14d34bcce48e8ca81565eb9b53f982fcf829d
admin <arnav.jain@dotsqures.com>
2025-09-19 16:57:35 +0530

    aaa

--- api/index.js
+++ api/index.js
@@ -2,13 +2,26 @@ import express from "express";
 import fs from "fs-extra";
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/api", async (req, res) => {
   try {
-    const data = await fs.readJson("products.json");
-    res.json(data);
+    const rawData = await fs.readJson("products.json");
+
+    const productsOnly = rawData.filter(
+      (p) => p.id && p.id.startsWith("gid://shopify/Product/")
+    );
+
+    const filteredProducts = productsOnly.filter((p) =>
+      ["ACTIVE", "DRAFT"].includes((p.status || "").toUpperCase())
+    );
+
+    res.json({
+      total: filteredProducts.length,
+      products: filteredProducts,
+    });
   } catch (err) {
+    console.error(err);
     res.status(500).json({ error: "Failed to load products.json" });
   }
 });
